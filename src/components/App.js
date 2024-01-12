ce178bad589beb4f2d5714cf735954eef53e798c
Milan <m26030820@gmail.com>
2024-01-12 09:09:39 +0100

    Learning useState Hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
+import { useState} from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increase(){
+    setCount(count + 1); 
+  }
+
+  function decrease(){
+    setCount(count - 1)
+  }
+
   return (
     <div className="App">
-      <h1>0</h1>
-      <button>+</button>
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
