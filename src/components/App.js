7251452b14cab844b9ca06b23eacd7c3590fd49a
Milan <m26030820@gmail.com>
2024-01-12 09:45:52 +0100

    Practice Hook React

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,48 @@
 import { useState} from "react";
 
+
 function App() {
-  const [count, setCount] = useState(0);
+  setInterval(updateTime, 1000);
 
-  function increase(){
-    setCount(count + 1); 
-  }
+  const now = new Date().toLocaleTimeString();
 
-  function decrease(){
-    setCount(count - 1)
+  //1st input is value and 2nd is a function to that value. "now" is the starting point
+  const [time, setTime] = useState(now);
+
+  function updateTime(){
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime)
   }
 
   return (
-    <div className="App">
-      <h1>{count}</h1>
-      <button onClick={increase}>+</button>
-      <button onClick={decrease}>-</button>
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
 
+
 export default App;
+
+
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   function increase(){
+//     setCount(count + 1); 
+//   }
+
+//   function decrease(){
+//     setCount(count - 1)
+//   }
+
+//   return (
+//     <div className="App">
+//       <h1>{count}</h1>
+//       <button onClick={increase}>+</button>
+//       <button onClick={decrease}>-</button>
+//     </div>
+//   );
+// }
